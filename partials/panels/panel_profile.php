92f094d1f972fa647a6fa309e0c833c0e721d592
bmodena <bmodena@sussind.com>
2015-09-08 14:03:59 -0600

    profile

--- partials/panels/panel_profile.php
+++ partials/panels/panel_profile.php
@@ -8,21 +8,21 @@
     <div class="panel-body">
       <div class="row">
 
-        <div id="profile-image" class="col-md-3">
+        <div id="profile-image" class="col-xs-6 col-sm-6 col-md-3">
             <img src="/public/images/profile.jpg" alt="name" />
         </div>
 
-        <div id="user-info" class="col-md-3">
+        <div id="user-info" class="col-xs-6 col-sm-6 col-md-3">
           <h4>Travis Rice </h4>
           <p>@Trice </p>
           <p>Last Active : Apr-12, 2016 </p>
           <p>Goal Progress: 52 of 80 </p>
         </div>
 
-        <div id="stats" class="col-md-6">
+        <div id="stats" class="col-sm-12 col-md-6">
           <div class="row">
 
-            <div class="col-md-4">
+            <div class="col-xs-4">
               <h6>Leaderboard</h6>
               <p class="number" id="leaderboard-count"><span class="timer" data-countto="231"> </span><i class="fa fa-caret-up increase"></i> </p>
 
@@ -31,7 +31,7 @@
 
             </div>
 
-            <div class="col-md-4">
+            <div class="col-xs-4">
               <h6>Vertical</h6>
               <p class="number" id="vertical"><span class="timer" data-countto="23,121"> </span></p>
 
@@ -40,7 +40,7 @@
 
             </div>
 
-            <div class="col-md-4">
+            <div class="col-xs-4">
               <h6>Lifts</h6>
               <p class="number" id="leaderboard-count"><span class="timer" data-countto="11"> </span></p>
 
